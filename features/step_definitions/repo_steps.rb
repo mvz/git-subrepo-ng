72d48bc457e73d7018e9c6b3bf9b2ebeac20d410
Matijs van Zuijlen <matijs@matijs.net>
2019-12-15 16:29:27 +0100

    Set up test repo using Rugged

--- features/step_definitions/repo_steps.rb
+++ features/step_definitions/repo_steps.rb
@@ -1,25 +1,30 @@
 Given("I have an existing git project named {string}") do |proj|
   create_directory proj
   cd proj do
-    system "git init"
+    repo = Rugged::Repository.init_at(".")
     write_file "README", "Hi!"
-    system "git add -A"
-    system "git commit -am 'Initial commit'"
+    index = repo.index
+    index.add "README"
+    Rugged::Commit.create(repo, tree: index.write_tree, message: "Initial commit",
+                          parents: [], update_ref: "HEAD")
   end
   @main_repo = proj
 end
 
 Given("I have a subdirectory {string} with commits") do |subdir|
   cd @main_repo do
+    repo = Rugged::Repository.new(".")
     create_directory subdir
     write_file "#{subdir}/a_file", "stuff"
-    system "git add -A"
-    system "git commit -am 'Add stuff in #{subdir}'"
+    index = repo.index
+    index.add "#{subdir}/a_file"
+    Rugged::Commit.create(repo, tree: index.write_tree, message: "Add stuff in #{subdir}",
+                          parents: [repo.head.target], update_ref: "HEAD")
   end
 end
 
 Given("I have an empty remote named {string}") do |remote|
   in_current_directory do
-    system "git init --bare #{remote}"
+    Rugged::Repository.init_at(remote, :bare)
   end
 end
