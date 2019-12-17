7db00bae82df0ab87c794d8974d5164f36eb37f1
Matijs van Zuijlen <matijs@matijs.net>
2019-12-17 21:28:58 +0100

    Remove generated failing spec

--- spec/subrepo_spec.rb
+++ spec/subrepo_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Subrepo do
   it "has a version number" do
     expect(Subrepo::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
