53ab2eaaac84185778a3c1f6c8969964b330f1f1
Matijs van Zuijlen <matijs@matijs.net>
2019-12-18 17:49:54 +0100

    Extract method map_commits

--- lib/subrepo/commands.rb
+++ lib/subrepo/commands.rb
@@ -95,60 +95,7 @@ module Subrepo
           raise "It seems #{split_branch_name} already exists. Remove it first"
         end
 
-        # Walk all commits that haven't been pushed yet
-        walker = Rugged::Walker.new(repo)
-        walker.push repo.head.target_id
-        walker.hide last_pushed_commit
-        commits = walker.to_a
-
-        commit_map = { last_pushed_commit => last_merged_commit }
-
-        last_commit = nil
-
-        commits.reverse_each do |commit|
-          # Fetch subrepo's tree
-          subtree = repo.lookup commit.tree[subdir][:oid]
-
-          # Filter out .gitrepo
-          builder = Rugged::Tree::Builder.new(repo)
-          subtree.filter { |it| it[:name] != ".gitrepo" }.each { |it| builder << it }
-          rewritten_tree_sha = builder.write
-          rewritten_tree = repo.lookup rewritten_tree_sha
-
-          # Map parent commits
-          parent_shas = commit.parents.map(&:oid)
-          target_parent_shas = parent_shas.map do |sha|
-            # TODO: Improve upon last_merged_commit as best guess
-            commit_map.fetch sha, last_merged_commit
-          end.uniq
-          target_parents = target_parent_shas.map { |sha| repo.lookup sha }
-
-          if target_parents.one?
-            target_parent = target_parents.first
-            diff = target_parent.tree.diff rewritten_tree
-            # If commit tree is no different from the target parent, map this
-            # commit to the target parent and skip to the next commit.
-            if diff.none?
-              commit_map[commit.oid] = target_parent.oid
-              next
-            end
-          end
-
-          # Commit has multiple mapped parents or is non-empty: We should
-          # create it in the target branch too.
-          options = {}
-          options[:tree] = rewritten_tree
-          options[:author] = commit.author
-          options[:committer] = commit.committer
-          options[:parents] = target_parents
-          options[:message] = commit.message
-
-          puts "Committing #{commit.summary}"
-
-          new_commit_sha = Rugged::Commit.create(repo, options)
-          commit_map[commit.oid] = new_commit_sha
-          last_commit = new_commit_sha
-        end
+        last_commit = map_commits(repo, subdir, last_pushed_commit, last_merged_commit)
 
         unless last_commit
           puts "No changes to push"
@@ -220,6 +167,64 @@ module Subrepo
       Rugged::Commit.create(repo, tree: index.write_tree,
                             message: "Initialize subrepo #{subdir}",
                             parents: [repo.head.target], update_ref: "HEAD")
+    end
+
+    def map_commits(repo, subdir, last_pushed_commit, last_merged_commit)
+      # Walk all commits that haven't been pushed yet
+      walker = Rugged::Walker.new(repo)
+      walker.push repo.head.target_id
+      walker.hide last_pushed_commit
+      commits = walker.to_a
+
+      commit_map = { last_pushed_commit => last_merged_commit }
+
+      last_commit = nil
+
+      commits.reverse_each do |commit|
+        # Fetch subrepo's tree
+        subtree = repo.lookup commit.tree[subdir][:oid]
+
+        # Filter out .gitrepo
+        builder = Rugged::Tree::Builder.new(repo)
+        subtree.filter { |it| it[:name] != ".gitrepo" }.each { |it| builder << it }
+        rewritten_tree_sha = builder.write
+        rewritten_tree = repo.lookup rewritten_tree_sha
+
+        # Map parent commits
+        parent_shas = commit.parents.map(&:oid)
+        target_parent_shas = parent_shas.map do |sha|
+          # TODO: Improve upon last_merged_commit as best guess
+          commit_map.fetch sha, last_merged_commit
+        end.uniq
+        target_parents = target_parent_shas.map { |sha| repo.lookup sha }
+
+        if target_parents.one?
+          target_parent = target_parents.first
+          diff = target_parent.tree.diff rewritten_tree
+          # If commit tree is no different from the target parent, map this
+          # commit to the target parent and skip to the next commit.
+          if diff.none?
+            commit_map[commit.oid] = target_parent.oid
+            next
+          end
+        end
+
+        # Commit has multiple mapped parents or is non-empty: We should
+        # create it in the target branch too.
+        options = {}
+        options[:tree] = rewritten_tree
+        options[:author] = commit.author
+        options[:committer] = commit.committer
+        options[:parents] = target_parents
+        options[:message] = commit.message
+
+        puts "Committing #{commit.summary}"
+
+        new_commit_sha = Rugged::Commit.create(repo, options)
+        commit_map[commit.oid] = new_commit_sha
+        last_commit = new_commit_sha
+      end
+      return last_commit
     end
   end
 end
