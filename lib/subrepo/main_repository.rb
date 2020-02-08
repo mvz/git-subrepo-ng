3385c061a7f4b8f35ba8d7b704033d613fd96893
Matijs van Zuijlen <matijs@matijs.net>
2020-02-08 15:36:41 +0100

    Extract class MainRepository

---
+++ lib/subrepo/main_repository.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rugged"
+
+module Subrepo
+  class MainRepository
+    attr_reader :repo
+
+    def initialize
+      @repo = Rugged::Repository.new(".")
+    end
+
+    def subrepos(recursive: false)
+      tree = repo.head.target.tree
+      subrepos = []
+      tree.walk_blobs do |path, blob|
+        next if blob[:name] != ".gitrepo"
+
+        unless recursive
+          next if subrepos.any? { |it| path.start_with? it }
+        end
+        subrepos << path
+      end
+      subrepos
+    end
+  end
+end
