e086ec73618de7360a65b3de3ad996177ac02190
Matijs van Zuijlen <matijs@matijs.net>
2020-03-15 18:47:04 +0100

    Remove superfluous brackets from step definitions

--- features/step_definitions/repo_steps.rb
+++ features/step_definitions/repo_steps.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-Given("I have an existing git project named {string}") do |proj|
+Given "I have an existing git project named {string}" do |proj|
   initialize_project proj
   @main_repo = proj
 end
 
-Given("I have a subdirectory {string} with commits") do |subdir|
+Given "I have a subdirectory {string} with commits" do |subdir|
   subdir_with_commits_in_project(@main_repo, subdir: subdir)
 end
 
-Given("I have an empty remote named {string}") do |remote|
+Given "I have an empty remote named {string}" do |remote|
   empty_remote(remote)
   @remote = remote
 end
@@ -24,7 +24,7 @@ Given "I have a git project {string} with subrepo {string} with remote {string}"
   @remote = remote
 end
 
-Given("I have a git project with a subrepo with a remote") do
+Given "I have a git project with a subrepo with a remote" do
   @main_repo = "foo"
   @subrepo = "bar"
   @remote = "baz"
@@ -41,7 +41,7 @@ Given "I have a remote named {string} with some commits" do |remote|
   remote_commit_add(full_remote, "other_file", "more stuff")
 end
 
-When("I add a new commit to the subrepo") do
+When "I add a new commit to the subrepo" do
   cd @main_repo do
     repo = Rugged::Repository.new(".")
     write_file "#{@subrepo}/other_file", "more stuff"
@@ -55,7 +55,7 @@ When("I add a new commit to the subrepo") do
   end
 end
 
-When("I add a new commit to the remote") do
+When "I add a new commit to the remote" do
   repo = Rugged::Repository.new expand_path(@remote)
   index = repo.index
   index.read_tree(repo.head.target.tree)
@@ -66,7 +66,7 @@ When("I add a new commit to the remote") do
                         parents: [repo.head.target], update_ref: "HEAD")
 end
 
-When("I create a branch with some commits in the main project") do
+When "I create a branch with some commits in the main project" do
   cd @main_repo do
     `git checkout -q -b unrelated-branch`
     write_file "another_main_file", "stuff"
@@ -79,13 +79,13 @@ When("I create a branch with some commits in the main project") do
   end
 end
 
-When("I merge in the main project branch") do
+When "I merge in the main project branch" do
   cd @main_repo do
     `git merge unrelated-branch`
   end
 end
 
-Then("the subrepo and the remote should have the same contents") do
+Then "the subrepo and the remote should have the same contents" do
   repo = Rugged::Repository.new expand_path(@remote)
   tree = repo.head.target.tree
   subrepo = expand_path @subrepo, @main_repo
@@ -100,17 +100,17 @@ Then("the subrepo and the remote should have the same contents") do
   end
 end
 
-Then("the remote's log should equal:") do |string|
+Then "the remote's log should equal:" do |string|
   log = get_log_from_repo(@remote)
   expect(log).to eq string
 end
 
-Then("the project's log should equal:") do |string|
+Then "the project's log should equal:" do |string|
   log = get_log_from_repo(@main_repo)
   expect(log).to eq string
 end
 
-Then("the subrepo configuration should contain the latest commit and parent") do
+Then "the subrepo configuration should contain the latest commit and parent" do
   remote_repo = Rugged::Repository.new expand_path(@remote)
   repo = Rugged::Repository.new expand_path(@main_repo)
   subrepo = expand_path @subrepo, @main_repo
--- features/step_definitions/subrepo_action_steps.rb
+++ features/step_definitions/subrepo_action_steps.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-Given("I have initialized and pushed the subrepo") do
-  cd(@main_repo) do
+Given "I have initialized and pushed the subrepo" do
+  cd @main_repo do
     Subrepo::Runner.new.run_init @subrepo, remote: "../#{@remote}", branch: "master"
     Subrepo::Runner.new.run_push @subrepo
   end
@@ -9,33 +9,33 @@ end
 
 When "I init the subrepo {string} with remote {string} and branch {string}" \
   do |subrepo, remote, branch|
-  cd(@main_repo) do
+  cd @main_repo do
     @subrepo = subrepo
     Subrepo::Runner.new.run_init @subrepo, remote: remote, branch: branch
   end
 end
 
-When("I push the subrepo {string}") do |subrepo|
-  cd(@main_repo) do
+When "I push the subrepo {string}" do |subrepo|
+  cd @main_repo do
     Subrepo::Runner.new.run_push subrepo
   end
 end
 
-When("I pull the subrepo with squashing( again)") do
-  cd(@main_repo) do
+When "I pull the subrepo with squashing( again)" do
+  cd @main_repo do
     Subrepo::Runner.new.run_pull @subrepo, squash: true
   end
 end
 
-When("I pull the subrepo without squashing( again)") do
-  cd(@main_repo) do
+When "I pull the subrepo without squashing( again)" do
+  cd @main_repo do
     Subrepo::Runner.new.run_pull @subrepo, squash: false
   end
 end
 
 When "I clone into {string} from the remote {string} with branch {string}" \
   do |subdir, remote, branch|
-  cd(@main_repo) do
+  cd @main_repo do
     @subrepo = subdir
     Subrepo::Runner.new.run_clone remote, @subrepo, branch: branch
   end
