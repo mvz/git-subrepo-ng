922d5ae07320eaee99615ab1a8e5b195eb4df873
Matijs van Zuijlen <matijs@matijs.net>
2020-04-25 19:49:30 +0200

    Extract SubRepository#full_commit_map to a new class

--- lib/subrepo/sub_repository.rb
+++ lib/subrepo/sub_repository.rb
@@ -4,6 +4,7 @@ require "rugged"
 require "tempfile"
 require "fileutils"
 require "shellwords"
+require "subrepo/commit_mapper"
 
 module Subrepo
   # SubRepository, represents a subrepo
@@ -204,6 +205,25 @@ module Subrepo
       @config ||= Config.new(subdir)
     end
 
+    def repo
+      @repo ||= main_repository.repo
+    end
+
+    def config_file_in_tree(tree)
+      subtree = tree_in_subrepo(tree)
+      subtree[".gitrepo"] if subtree
+    end
+
+    def calculate_subtree(commit)
+      subtree = tree_in_subrepo(commit.tree)
+      builder = Rugged::Tree::Builder.new(repo)
+      # Filter out .gitrepo
+      subtree&.reject { |it| it[:name] == ".gitrepo" }&.each { |it| builder << it }
+
+      rewritten_tree_sha = builder.write
+      repo.lookup rewritten_tree_sha
+    end
+
     private
 
     def subref
@@ -329,7 +349,7 @@ module Subrepo
       # Map parent commits
       target_parent_shas = parents.map do |parent|
         # TODO: Improve upon last_merged_commit as best guess
-        commit_map.fetch parent.oid, last_merged_commit
+        commit_map[parent.oid] || last_merged_commit
       end.uniq.compact
       if commit_map[commit.oid]
         mapped_oid = commit_map[commit.oid]
@@ -402,95 +422,8 @@ module Subrepo
       rewritten_tree
     end
 
-    def config_file_in_tree(tree)
-      subtree = tree_in_subrepo(tree)
-      subtree[".gitrepo"] if subtree
-    end
-
     def commit_map
-      @commit_map ||= full_commit_map
-    end
-
-    def full_commit_map
-      commit_map = {}
-      walker = Rugged::Walker.new(repo)
-      walker.push repo.head.target_id
-      walker.to_a.reverse_each do |commit|
-        parent = commit.parents[0] or next
-        current = config_file_in_tree(commit.tree) or next
-
-        previous = config_file_in_tree(parent.tree)
-        next if previous && current[:oid] == previous[:oid]
-
-        config = config_from_blob_oid current[:oid]
-        pushed_commit_oid = config["subrepo.parent"] or next
-        merged_commit_oid = config["subrepo.commit"]
-
-        remote_commit_tree = repo.lookup(merged_commit_oid).tree
-
-        sub_walker = Rugged::Walker.new(repo)
-        sub_walker.push pushed_commit_oid
-        # TODO: Maybe make sure we don't hide pushed_commit_oid
-        commit_map.each_key { |oid| sub_walker.hide oid }
-
-        dependent_commits = sub_walker.to_a
-
-        dependent_commits.reverse_each do |sub_commit|
-          sub_commit_tree = calculate_subtree(sub_commit)
-
-          # TODO: Maybe this section only makes sense for the first commit we handle?
-          # Compare trees for earlier commits with the current tree
-          if sub_commit_tree.oid == remote_commit_tree.oid
-            commit_map[sub_commit.oid] = merged_commit_oid
-            next
-          end
-
-          # TODO: Also check children of the parents' mapped commits
-          # Compare trees for earlier commits with their parent trees
-          sub_commit.parents.each do |sub_parent|
-            mapped_parent_oid = commit_map[sub_parent.oid]
-            next unless mapped_parent_oid
-
-            sub_parent_tree = calculate_subtree(sub_parent)
-            next unless sub_commit_tree.oid == sub_parent_tree.oid
-
-            commit_map[sub_commit.oid] = mapped_parent_oid
-            break
-          end
-        end
-
-        last_pushed_commit = repo.lookup pushed_commit_oid
-        last_pushed_commit_tree = calculate_subtree(last_pushed_commit)
-        if last_pushed_commit_tree.oid == remote_commit_tree.oid
-          commit_map[pushed_commit_oid] = merged_commit_oid
-        end
-
-        # Map the commit containing a change to .gitrepo.
-        # If its subtree is equal to remote tree, we can safely map it.
-        # Otherwise, if it has one parent, assume it's a squash merge commit.
-        if commit.parents.count == 1 ||
-            calculate_subtree(commit).oid == remote_commit_tree.oid
-          commit_map[commit.oid] = merged_commit_oid
-        end
-      end
-      commit_map
-    end
-
-    def config_from_blob_oid(oid)
-      tmp = Tempfile.new("config")
-      tmp.write repo.lookup(oid).text
-      tmp.close
-      Rugged::Config.new(tmp.path)
-    end
-
-    def calculate_subtree(commit)
-      subtree = tree_in_subrepo(commit.tree)
-      builder = Rugged::Tree::Builder.new(repo)
-      # Filter out .gitrepo
-      subtree&.reject { |it| it[:name] == ".gitrepo" }&.each { |it| builder << it }
-
-      rewritten_tree_sha = builder.write
-      repo.lookup rewritten_tree_sha
+      @commit_map ||= CommitMapper.map_commits(self)
     end
 
     def calculate_patch(rewritten_tree, target_parent_tree)
@@ -532,10 +465,6 @@ module Subrepo
 
     def subdir_parts
       @subdir_parts ||= subdir.split(%r{/+})
-    end
-
-    def repo
-      @repo ||= main_repository.repo
     end
 
     def hexify(char)
